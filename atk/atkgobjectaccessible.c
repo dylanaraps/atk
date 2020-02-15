661c218e381c0c031188aa1339e23206d8f14059
Dylan Araps <dylan.araps@gmail.com>
2020-02-15 12:53:51 +0200

    docs: update

--- atk/atkgobjectaccessible.c
+++ atk/atkgobjectaccessible.c
@@ -18,22 +18,10 @@
  */
 
 #include "config.h"
-
 #include <atk/atkgobjectaccessible.h>
 #include <atk/atkregistry.h>
 #include <atk/atkutil.h>
 
-/**
- * SECTION:atkgobjectaccessible
- * @Short_description: This object class is derived from AtkObject and
- *  can be used as a basis implementing accessible objects.
- * @Title:AtkGObjectAccessible
- *
- * This object class is derived from AtkObject. It can be used as a
- * basis for implementing accessible objects for GObjects which are
- * not derived from GtkWidget. One example of its use is in providing
- * an accessible object for GnomeCanvasItem in the GAIL library.
- */
 static void       atk_gobject_accessible_class_init       (AtkGObjectAccessibleClass   *klass);
 static void       atk_real_gobject_accessible_initialize  (AtkObject         *atk_obj,
                                                            gpointer          data);
@@ -43,9 +31,7 @@ static GQuark quark_accessible_object = 0;
 static GQuark quark_object = 0;
 static gpointer parent_class = NULL;
 
-GType
-atk_gobject_accessible_get_type (void)
-{
+GType atk_gobject_accessible_get_type(void) {
   static GType type = 0;
 
   if (!type)
@@ -71,136 +57,24 @@ atk_gobject_accessible_get_type (void)
   return type;
 }
 
-/**
- * atk_gobject_accessible_for_object:
- * @obj: a #GObject
- *
- * Gets the accessible object for the specified @obj.
- *
- * Returns: (transfer none): a #AtkObject which is the accessible object for
- * the @obj
- **/
-AtkObject*
-atk_gobject_accessible_for_object (GObject *obj)
-{
-  AtkObject* accessible;
-
-  g_return_val_if_fail (G_IS_OBJECT (obj), NULL);
-  /* See if we have a cached accessible for this object */
-
-  accessible = quark_accessible_object ? g_object_get_qdata (obj, quark_accessible_object) : NULL;
-
-  if (!accessible)
-    {
-      AtkObjectFactory *factory;
-      AtkRegistry *default_registry;
-
-      default_registry = atk_get_default_registry ();
-      factory = atk_registry_get_factory (default_registry, 
-                                          G_OBJECT_TYPE (obj));
-      accessible = atk_object_factory_create_accessible (factory,
-                                                         obj);
-      if (!ATK_IS_GOBJECT_ACCESSIBLE (accessible))
-        {
-          /*
-           * The AtkObject which was created was not a AtkGObjectAccessible
-           */
-          g_object_weak_ref (obj,
-                             (GWeakNotify) g_object_unref,
-                             accessible); 
-          if (!quark_accessible_object)
-            quark_accessible_object = g_quark_from_static_string ("accessible-object");
-        }
-      g_object_set_qdata (obj, quark_accessible_object, accessible);
-    }
-  return accessible;
+AtkObject* atk_gobject_accessible_for_object (GObject *obj) {
+    return NULL;
 }
 
-/**
- * atk_gobject_accessible_get_object:
- * @obj: a #AtkGObjectAccessible
- *
- * Gets the GObject for which @obj is the accessible object.
- *
- * Returns: (transfer none): a #GObject which is the object for which @obj is
- * the accessible object
- **/
-GObject *
-atk_gobject_accessible_get_object (AtkGObjectAccessible *obj)
-{
-  g_return_val_if_fail (ATK_IS_GOBJECT_ACCESSIBLE (obj), NULL);
-
-  return g_object_get_qdata (G_OBJECT (obj), quark_object);
+GObject * atk_gobject_accessible_get_object (AtkGObjectAccessible *obj){
+  return NULL;
 }
  
-static void
-atk_real_gobject_accessible_initialize (AtkObject  *atk_obj,
-                                        gpointer   data)
-{
-  AtkGObjectAccessible *atk_gobj;
-
-  atk_gobj = ATK_GOBJECT_ACCESSIBLE (atk_obj);
-
-  g_object_set_qdata (G_OBJECT (atk_gobj), quark_object, data);
-  atk_obj->layer = ATK_LAYER_WIDGET;
-
-  g_object_weak_ref (data,
-                     (GWeakNotify) atk_gobject_accessible_object_gone_cb,
-                     atk_gobj);
+static void atk_real_gobject_accessible_initialize (AtkObject  *atk_obj,
+                                        gpointer   data) {
 }
 
-static void
-atk_gobject_accessible_object_gone_cb (gpointer  data)
-{
-  GObject *object;
-
-  g_return_if_fail (ATK_IS_GOBJECT_ACCESSIBLE (data));
-
-  object = atk_gobject_accessible_get_object (data);
-  if (object)
-      g_object_set_qdata (object, quark_accessible_object, NULL);
-
-  g_object_set_qdata (G_OBJECT (data), quark_object, NULL);
-  atk_object_notify_state_change (ATK_OBJECT (data), ATK_STATE_DEFUNCT,
-                                  TRUE); 
-  g_object_unref (data);
+static void atk_gobject_accessible_object_gone_cb (gpointer  data) {
 }
 
-static void
-atk_gobject_accessible_dispose (GObject *atk_obj)
-{
-   GObject *obj = atk_gobject_accessible_get_object (ATK_GOBJECT_ACCESSIBLE (atk_obj));
-
-   if (obj) {
-      g_object_set_qdata (obj, quark_accessible_object, NULL);
-      g_object_weak_unref (obj,
-                           (GWeakNotify) atk_gobject_accessible_object_gone_cb,
-                           atk_obj);
-
-      g_object_set_qdata (atk_obj, quark_object, NULL);
-      atk_object_notify_state_change (ATK_OBJECT (atk_obj), ATK_STATE_DEFUNCT,
-                                      TRUE); 
-   }
-
-   G_OBJECT_CLASS (parent_class)->dispose (atk_obj);
+static void atk_gobject_accessible_dispose (GObject *atk_obj) {
 }
 
-static void
-atk_gobject_accessible_class_init (AtkGObjectAccessibleClass *klass)
+static void atk_gobject_accessible_class_init (AtkGObjectAccessibleClass *klass)
 { 
-  AtkObjectClass *class;
-  GObjectClass *object_class;
-
-  class = ATK_OBJECT_CLASS (klass);
-
-  parent_class = g_type_class_peek_parent (klass);
-
-  class->initialize = atk_real_gobject_accessible_initialize;
-
-  object_class = G_OBJECT_CLASS (klass);
-  object_class->dispose = atk_gobject_accessible_dispose;
-
-  if (!quark_accessible_object)
-    quark_accessible_object = g_quark_from_static_string ("accessible-object");
-  quark_object = g_quark_from_static_string ("object-for-accessible");
 }
--- atk/atkhyperlink.c
+++ atk/atkhyperlink.c
@@ -21,20 +21,6 @@
 #include "atkhyperlink.h"
 #include <glib/gi18n-lib.h>
 
-/**
- * SECTION:atkhyperlink
- * @Short_description: An ATK object which encapsulates a link or set
- *  of links in a hypertext document.
- * @Title:AtkHyperlink
- *
- * An ATK object which encapsulates a link or set of links (for
- * instance in the case of client-side image maps) in a hypertext
- * document.  It may implement the AtkAction interface.  AtkHyperlink
- * may also be used to refer to inline embedded content, since it
- * allows specification of a start and end offset within the host
- * AtkHypertext object.
- */
-
 enum
 {
   LINK_ACTIVATED,
@@ -68,9 +54,7 @@ static guint atk_hyperlink_signals[LAST_SIGNAL] = { 0, };
 
 static gpointer  parent_class = NULL;
 
-GType
-atk_hyperlink_get_type (void)
-{
+GType atk_hyperlink_get_type(void) {
   static GType type = 0;
 
   if (!type)
@@ -101,320 +85,51 @@ atk_hyperlink_get_type (void)
   return type;
 }
 
-static void
-atk_hyperlink_class_init (AtkHyperlinkClass *klass)
-{
-  GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
-
-  parent_class = g_type_class_peek_parent (klass);
-
-  gobject_class->get_property = atk_hyperlink_real_get_property;
-
-  klass->link_activated = NULL;
-
-  /**
-   * AtkHyperlink:selected-link:
-   *
-   * Selected link
-   *
-   * Deprecated: 1.8: Please use ATK_STATE_FOCUSABLE for all links, and
-   * ATK_STATE_FOCUSED for focused links.
-   */
-  g_object_class_install_property (gobject_class,
-                                   PROP_SELECTED_LINK,
-                                   g_param_spec_boolean ("selected-link",
-                                                         _("Selected Link"),
-                                                         _("Specifies whether the AtkHyperlink object is selected"),
-                                                         FALSE,
-                                                         G_PARAM_READABLE));
-  g_object_class_install_property (gobject_class,
-                                   PROP_NUMBER_ANCHORS,
-                                   g_param_spec_int ("number-of-anchors",
-                                                     _("Number of Anchors"),
-                                                     _("The number of anchors associated with the AtkHyperlink object"),
-                                                     0,
-                                                     G_MAXINT,
-                                                     0,
-                                                     G_PARAM_READABLE));
-  g_object_class_install_property (gobject_class,
-                                   PROP_END_INDEX,
-                                   g_param_spec_int ("end-index",
-                                                     _("End index"),
-                                                     _("The end index of the AtkHyperlink object"),
-                                                     0,
-                                                     G_MAXINT,
-                                                     0,
-                                                     G_PARAM_READABLE));
-  g_object_class_install_property (gobject_class,
-                                   PROP_START_INDEX,
-                                   g_param_spec_int ("start-index",
-                                                     _("Start index"),
-                                                     _("The start index of the AtkHyperlink object"),
-                                                     0,
-                                                     G_MAXINT,
-                                                     0,
-                                                     G_PARAM_READABLE));
-
-  /**
-   * AtkHyperlink::link-activated:
-   * @atkhyperlink: the object which received the signal.
-   *
-   * The signal link-activated is emitted when a link is activated.
-   */
-  atk_hyperlink_signals[LINK_ACTIVATED] =
-    g_signal_new ("link_activated",
-                  G_TYPE_FROM_CLASS (klass),
-                  G_SIGNAL_RUN_LAST,
-                  G_STRUCT_OFFSET (AtkHyperlinkClass, link_activated),
-                  NULL, NULL,
-                  g_cclosure_marshal_VOID__VOID,
-                  G_TYPE_NONE,
-                  0);
-
+static void atk_hyperlink_class_init (AtkHyperlinkClass *klass) {
 }
 
-static void
-atk_hyperlink_init  (AtkHyperlink        *link,
-                     AtkHyperlinkClass   *klass)
-{
+static void atk_hyperlink_init(AtkHyperlink *link, AtkHyperlinkClass *klass) {
 }
 
-static void
-atk_hyperlink_real_get_property (GObject    *object,
+static void atk_hyperlink_real_get_property(GObject *object,
                                  guint      prop_id,
                                  GValue     *value,
                                  GParamSpec *pspec)
 {
-  AtkHyperlink* link;
-
-  link = ATK_HYPERLINK (object);
-
-  switch (prop_id)
-    {
-    case PROP_SELECTED_LINK:
-      // This property is deprecated, also the method to get the value
-      g_value_set_boolean (value, FALSE);
-      break;
-    case PROP_NUMBER_ANCHORS:
-      g_value_set_int (value,  atk_hyperlink_get_n_anchors (link));
-      break;
-    case PROP_END_INDEX:
-      g_value_set_int (value, atk_hyperlink_get_end_index (link));
-      break;
-    case PROP_START_INDEX:
-      g_value_set_int (value, atk_hyperlink_get_start_index (link));
-      break;
-    default:
-      break;
-    }
 }
 
-/**
- * atk_hyperlink_get_uri:
- * @link_: an #AtkHyperlink
- * @i: a (zero-index) integer specifying the desired anchor
- *
- * Get a the URI associated with the anchor specified 
- * by @i of @link_. 
- *
- * Multiple anchors are primarily used by client-side image maps.
- *
- * Returns: a string specifying the URI 
- **/
-gchar*
-atk_hyperlink_get_uri (AtkHyperlink *link,
-                       gint         i)
-{
-  AtkHyperlinkClass *klass;
-
-  g_return_val_if_fail (ATK_IS_HYPERLINK (link), NULL);
-
-  klass = ATK_HYPERLINK_GET_CLASS (link);
-  if (klass->get_uri)
-    return (klass->get_uri) (link, i);
-  else
+gchar* atk_hyperlink_get_uri (AtkHyperlink *link, gint i) {
     return NULL;
 }
 
-/**
- * atk_hyperlink_get_object:
- * @link_: an #AtkHyperlink
- * @i: a (zero-index) integer specifying the desired anchor
- *
- * Returns the item associated with this hyperlinks nth anchor.
- * For instance, the returned #AtkObject will implement #AtkText
- * if @link_ is a text hyperlink, #AtkImage if @link_ is an image
- * hyperlink etc. 
- * 
- * Multiple anchors are primarily used by client-side image maps.
- *
- * Returns: (transfer none): an #AtkObject associated with this hyperlinks
- * i-th anchor
- **/
-AtkObject*
-atk_hyperlink_get_object (AtkHyperlink *link,
-                          gint         i)
-{
-  AtkHyperlinkClass *klass;
-
-  g_return_val_if_fail (ATK_IS_HYPERLINK (link), NULL);
-
-  klass = ATK_HYPERLINK_GET_CLASS (link);
-  if (klass->get_object)
-    return (klass->get_object) (link, i);
-  else
+AtkObject* atk_hyperlink_get_object (AtkHyperlink *link, gint i) {
     return NULL;
 }
 
-/**
- * atk_hyperlink_get_end_index:
- * @link_: an #AtkHyperlink
- *
- * Gets the index with the hypertext document at which this link ends.
- *
- * Returns: the index with the hypertext document at which this link ends
- **/
-gint
-atk_hyperlink_get_end_index (AtkHyperlink *link)
-{
-  AtkHyperlinkClass *klass;
-
-  g_return_val_if_fail (ATK_IS_HYPERLINK (link), 0);
-
-  klass = ATK_HYPERLINK_GET_CLASS (link);
-  if (klass->get_end_index)
-    return (klass->get_end_index) (link);
-  else
+gint atk_hyperlink_get_end_index (AtkHyperlink *link) {
     return 0;
 }
 
-/**
- * atk_hyperlink_get_start_index:
- * @link_: an #AtkHyperlink
- *
- * Gets the index with the hypertext document at which this link begins.
- *
- * Returns: the index with the hypertext document at which this link begins
- **/
-gint
-atk_hyperlink_get_start_index (AtkHyperlink *link)
-{
-  AtkHyperlinkClass *klass;
-
-  g_return_val_if_fail (ATK_IS_HYPERLINK (link), 0);
-
-  klass = ATK_HYPERLINK_GET_CLASS (link);
-  if (klass->get_start_index)
-    return (klass->get_start_index) (link);
-  else
+gint atk_hyperlink_get_start_index (AtkHyperlink *link) {
     return 0;
 }
 
-/**
- * atk_hyperlink_is_valid:
- * @link_: an #AtkHyperlink
- *
- * Since the document that a link is associated with may have changed
- * this method returns %TRUE if the link is still valid (with
- * respect to the document it references) and %FALSE otherwise.
- *
- * Returns: whether or not this link is still valid
- **/
-gboolean
-atk_hyperlink_is_valid (AtkHyperlink *link)
-{
-  AtkHyperlinkClass *klass;
-
-  g_return_val_if_fail (ATK_IS_HYPERLINK (link), FALSE);
-
-  klass = ATK_HYPERLINK_GET_CLASS (link);
-  if (klass->is_valid)
-    return (klass->is_valid) (link);
-  else
+gboolean atk_hyperlink_is_valid (AtkHyperlink *link) {
     return FALSE;
 }
 
-/**
- * atk_hyperlink_is_inline:
- * @link_: an #AtkHyperlink
- *
- * Indicates whether the link currently displays some or all of its
- *           content inline.  Ordinary HTML links will usually return
- *           %FALSE, but an inline &lt;src&gt; HTML element will return
- *           %TRUE.
- *
- * Returns: whether or not this link displays its content inline.
- *
- **/
-gboolean
-atk_hyperlink_is_inline (AtkHyperlink *link)
-{
-  AtkHyperlinkClass *klass;
-
-  g_return_val_if_fail (ATK_IS_HYPERLINK (link), FALSE);
-
-  klass = ATK_HYPERLINK_GET_CLASS (link);
-  if (klass->link_state)
-    return (klass->link_state (link) & ATK_HYPERLINK_IS_INLINE);
-  else
+gboolean atk_hyperlink_is_inline (AtkHyperlink *link) {
     return FALSE;
 }
 
-/**
- * atk_hyperlink_get_n_anchors:
- * @link_: an #AtkHyperlink
- *
- * Gets the number of anchors associated with this hyperlink.
- *
- * Returns: the number of anchors associated with this hyperlink
- **/
-gint
-atk_hyperlink_get_n_anchors (AtkHyperlink *link)
-{
-  AtkHyperlinkClass *klass;
-
-  g_return_val_if_fail (ATK_IS_HYPERLINK (link), 0);
-
-  klass = ATK_HYPERLINK_GET_CLASS (link);
-  if (klass->get_n_anchors)
-    return (klass->get_n_anchors) (link);
-  else
+gint atk_hyperlink_get_n_anchors (AtkHyperlink *link) {
     return 0;
 }
 
-/**
- * atk_hyperlink_is_selected_link:
- * @link_: an #AtkHyperlink
- *
- * Determines whether this AtkHyperlink is selected
- *
- * Since: 1.4
- *
- * Deprecated: 1.8: Please use ATK_STATE_FOCUSABLE for all links,
- * and ATK_STATE_FOCUSED for focused links.
- *
- * Returns: True if the AtkHyperlink is selected, False otherwise
- **/
-gboolean
-atk_hyperlink_is_selected_link (AtkHyperlink *link)
-{
-  AtkHyperlinkClass *klass;
-
-  g_return_val_if_fail (ATK_IS_HYPERLINK (link), FALSE);
-
-  klass = ATK_HYPERLINK_GET_CLASS (link);
-  if (klass->is_selected_link)
-    return (klass->is_selected_link) (link);
-  else
+gboolean atk_hyperlink_is_selected_link (AtkHyperlink *link) {
     return FALSE;
 }
 
 static void atk_hyperlink_action_iface_init (AtkActionIface *iface)
 {
-  /*
-   * We do nothing here
-   *
-   * When we come to derive a class from AtkHyperlink we will provide an
-   * implementation of the AtkAction interface. 
-   */
 }
